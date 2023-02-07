a158675ee0b5a3458e5f93ae921bcd030969a728
Atendimento <atendimento@mobiohsistemas.com.br>
2023-02-07 17:15:56 -0300

    organizando

--- src/Components/Container/Css/ContainerStyle.jsx
+++ src/Components/Container/Css/ContainerStyle.jsx
@@ -5,7 +5,5 @@ display:flex;
 flex-direction:column;
 width: 1920px;
 height: 1476px;
-
-
 background: #F3F5F7;
 `;
--- src/Components/Container/Css/HeartStyle.jsx
+++ src/Components/Container/Css/HeartStyle.jsx
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 
 export const HeartStyle = styled.img`
-    display:inline-block;
+    display: inline-block;
+    width:22px;
+    height:20px;
     padding-left:870px;
     padding-top:10px;
-    
- 
-`
+`;
