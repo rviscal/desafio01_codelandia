6fd279a461c8d0284ab45226f753dc50afc59bac
Atendimento <atendimento@mobiohsistemas.com.br>
2023-02-07 08:52:54 -0300

    Primeiro

---
+++ src/Components/TopPage/Css/Style.jsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+
+ export const Top = styled.div`
+width: 1920px;
+height: 250px;
+left: 0px;
+top: 0px;
+background: linear-gradient(88.27deg, #574AE8 0%, #3EA1DB 100%);
+
+`;
+
+export const Codelandia = styled.h1`
+position: absolute;
+width: 133px;
+height: 30px;
+left: 509px;
+top: 41px;
+font-family: 'Lexend Deca';
+font-style: normal;
+font-weight: 400;
+font-size: 24px;
+line-height: 30px;
+text-align: right;
+color: #FFFFFF;
+
+`;
+
+export const Blog = styled.h1`
+position: absolute;
+width: 52px;
+height: 30px;
+left: 1358px;
+top: 41px;
+font-family: 'Lexend Deca';
+font-style: normal;
+font-weight: 400;
+font-size: 24px;
+line-height: 30px;
+text-align: right;
+color: #FFFFFF;
+ 
+`;
+
+
+
+
---
+++ src/Components/TopPage/TopPage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Top ,Codelandia, Blog} from "./Css/Style";
+
+
+function TopPage (){
+    return(
+
+    <Top>
+        <Codelandia>Codelândia</Codelandia>
+        <Blog>Blog</Blog>
+        
+    </Top>
+    
+     
+    ) 
+
+
+}
+export default TopPage
