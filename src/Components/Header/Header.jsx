bfeca4cdcab954200c5fa8e4123a7b6df017d035
Atendimento <atendimento@mobiohsistemas.com.br>
2023-02-08 16:15:04 -0300

    filter

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,25 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import { Top, Codelandia, Blog, Seach, ImgS } from "./Css/Style";
-import { Input } from "./Css/StyleInput"
-import seaching from "../../Assets/seach.png"
-
+import { Input } from "./Css/StyleInput";
+import seaching from "../../Assets/seach.png";
 
 
 
 function Header() {
   
-
+const [search,setSearch] = useState();
     return (
         <Top>
             <Codelandia>Codelândia</Codelandia>
             <Blog>Blog</Blog>
             <Seach>
                 <ImgS src={seaching} />
-                <Input type='text' placeholder="Pesquise no Blog" ></Input>
+                <Input type='text' placeholder="Pesquise no Blog" id="inputHeader" value={search} onChange={(e)=> setSearch(e.target.value)} ></Input>
             </Seach>
         </Top>
-            )
+    )
 }
-
-
 export default Header
