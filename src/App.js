6907d14101b5ae27e3e9bb823a0cc90f903f91b7
Atendimento <atendimento@mobiohsistemas.com.br>
2023-02-08 17:05:35 -0300

    asda

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-
 import './App.css';
 import Header from './Components/Header/Header'
 import Container from './Components/Container/Container';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [pesquisa, setPesquisa] = useState('');
   return (
     <>
-    <Header />
-    <Container />
+      <Header pesquisa={pesquisa} setPesquisa={setPesquisa} />
+      <Container pesquisa={pesquisa} setPesquisa={setPesquisa} />
     </>
   );
 }
