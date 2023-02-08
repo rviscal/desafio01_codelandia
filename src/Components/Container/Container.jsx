0b54e2ac21d0774d75e59d4da130b6f064b8a0b2
Atendimento <atendimento@mobiohsistemas.com.br>
2023-02-08 09:32:36 -0300

    Teste lista

--- src/Components/Container/Container.jsx
+++ src/Components/Container/Container.jsx
@@ -1,42 +1,35 @@
 import React from "react";
 import {ContainerStyle,} from "./Css/ContainerStyle";
 import { Containerbox1,Ptext1,Htext1, Ptext2 } from "./Css/ContainerBox1";
-import { Containerbox2} from "./Css/ContainerBox2";
-import { Containerbox3} from "./Css/ContainerBox3";
-import { Containerbox4} from "./Css/ContainerBox4";
-import Heart from "./Heart";
+
+
 
 
     function Container () {
+        const itens = [
+        {ptex1:'02 de jul, 2021'},
+        {htex1:'Agora é oficial: o Windows 11 está vindo'},
+        {ptex2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est. Nam vitae finibus ante. Duis lobortis tellus vel diam fringilla, eu ullamcorper ex iaculis.'},
+        {ptex1:'02 de jul, 2021'}
+        ];
+
+        const listitem=itens.map(
+            (item,i)=>
+                <>
+                
+                <Ptext1 key={i}>{item.ptex1}</Ptext1>
+                <Htext1 key={i}>{item.htex1}</Htext1>
+                <Ptext2 key={i}>{item.ptex2}</Ptext2>
+                
+                </>
+                
+        )
         return(
-            <ContainerStyle>
-                <Containerbox1>
-                    <Heart/>
-                    <Ptext1>02 de jul, 2021</Ptext1>   
-                    <Htext1>Agora é oficial: o Windows 11 está vindo</Htext1>
-                    <Ptext2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est. Nam vitae finibus ante. Duis lobortis tellus vel diam fringilla, eu ullamcorper ex iaculis.</Ptext2>
-                </Containerbox1>
-            <Containerbox2>
-                <Heart/>
-                <Ptext1>02 de jul, 2021</Ptext1>
-                <Htext1>Tim Berners-Lee vai leiloar código-fonte da web</Htext1>
-                <Ptext2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est. Nam vitae finibus ante. Duis lobortis tellus vel diam fringilla, eu ullamcorper ex iaculis. Praesent et auctor justo. Vestibulum nisl orci, lacinia venenatis leo sit amet, pulvinar tincidunt risus. Phasellus nisl dui, cursus a lectus et, interdum ullamcorper libero.</Ptext2>
-            </Containerbox2>
-            <Containerbox3>
-                <Heart/>
-                <Ptext1>02 de jul, 2021</Ptext1>
-                <Htext1>Tem Firefox novo no pedaço e você vai querer migrar</Htext1>
-                <Ptext2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est.</Ptext2>
-            </Containerbox3>
-            <Containerbox4>
-                <Heart/>
-                <Ptext1>02 de jul, 2021</Ptext1>
-                <Htext1>John McAfee, criador do antivírus McAfee, morre</Htext1>
-                <Ptext2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est. Nam vitae finibus ante. Duis lobortis tellus vel diam fringilla, eu ullamcorper ex iaculis. Praesent et auctor justo. Vestibulum nisl orci, lacinia venenatis leo sit amet, pulvinar tincidunt risus. Phasellus nisl dui, cursus a lectus et, interdum ullamcorper libero.</Ptext2>
-            </Containerbox4>
-
-
-            </ContainerStyle>
+           <ContainerStyle>
+            <Containerbox1>
+              {listitem}
+              </Containerbox1>
+           </ContainerStyle>
         )
     }
     export default Container
--- src/Components/Container/Css/ContainerBox1.jsx
+++ src/Components/Container/Css/ContainerBox1.jsx
@@ -2,8 +2,6 @@ import styled from "styled-components";
 
 export const Containerbox1 = styled.div`
 margin:auto;
-width: 900px;
-height: 195px;
 background: #ffffff;
 box-shadow: 0px 0px 10px rgba(19, 19, 31, 0.05);
 border-radius: 5px;
@@ -12,19 +10,13 @@ export const Ptext1 = styled.p`
 display:inline;
 margin-left: 30px;
 font-family: 'Lexend Deca';
-font-style: normal;
-font-weight: 400;
 font-size: 16px;
-line-height: 20px;
 text-align: left;
 color: #717171; 
 `;
 export const Htext1 = styled.h1`
 font-family: 'Lexend Deca';
-font-style: normal;
-font-weight: 400;
 font-size: 26px;
-line-height: 32px;
 text-align: left;
 margin-left: 30px;
 color: #1A202C;
@@ -32,11 +24,9 @@ color: #1A202C;
 
 export const Ptext2 = styled.p`
 margin-left: 30px;
+margin-right: 30px;
 font-family: 'Lexend Deca';
-font-style: normal;
-font-weight: 400;
 font-size: 16px;
-line-height: 20px;
 text-align: left;
 color: #717171;  
 `;
